a026b7214ebb55ba78b2012dd93d755cb9b62518
Geoffrey Sangston <geoffreysangston@gmail.com>
2020-05-17 18:35:44 -0400

    Adds initial (non-functioning) version of CPForwardPas and CPBackwardPass

---
+++ test/test_tnn/test_tnn.py
@@ -0,0 +1,55 @@
+import tnn 
+import torch
+
+X = 128
+Y = 512
+
+R = 8
+S = 3
+T = 5
+H = 1024
+W = 1024
+
+
+U = torch.rand(X, Y, S)
+
+K = torch.rand(H, W, S, T)
+# obtained by cp decomp
+K0 = torch.rand(S, R)
+K1 = torch.rand(H, W, R)
+K2 = torch.rand(R, T)
+
+
+# from paper
+def CPForwardPass(U, K0, K1, K2):
+	U0 = tnn.conv_einsum("xys, sr -> xyr", U, K0)
+	U1 = tnn.conv_einsum("hwr, hwr -> hwr | hw", U0, K1) # here we change xy to hw
+	V = tnn.conv_einsum("hwr, rt -> hwt", U1, K2)
+	
+	return [U0, U1, V]
+
+
+
+dLdV = torch.rand(H, W, T)
+dLdK1 = torch.rand(H, W, R)
+dLdK2 = torch.rand(R, T)
+dLdU0 = torch.rand(X, Y, R)
+dLdU1 = torch.rand(H, W, R)
+
+# from paper
+def CPBackwardPass(K1, K2, U1, dLdV):
+	dLdU1 = tnn.conv_einsum("hwt, tr -> hwr", dLdV, K2.permute(1, 0)) 
+	dLdU0 = tnn.conv_einsum("hwr, hwr -> hwr | hw", dLdU1, K1)
+
+	dLdK2 = tnn.conv_einsum("hwr, hwt -> rt", U1, dLdV)	
+	dLdK1 = ?? email jiahao
+	dLdK0 = tnn.conv_einsum("hws, hwr -> sr", U, dLdU0) 
+			
+	return [dLdU0, dLdU1, dldK0, dLdK1, dLdK2]
+
+
+
+
+
+
+
